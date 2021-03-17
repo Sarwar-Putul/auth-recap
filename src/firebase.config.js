9959a14426ebd0a39ad9712ea005a65b6dcc797b
Sarwar-Putul <sarwarputul@gmail.com>
2021-03-18 00:31:17 +0600

    done

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATvlvs8t1jd61Q-7-OEDps1wL5w7XgVdU",
+    authDomain: "auth-recap-cae51.firebaseapp.com",
+    projectId: "auth-recap-cae51",
+    storageBucket: "auth-recap-cae51.appspot.com",
+    messagingSenderId: "901973240078",
+    appId: "1:901973240078:web:cab131d3ad9603eb278f5f"
+  };
+
+  export default firebaseConfig;
